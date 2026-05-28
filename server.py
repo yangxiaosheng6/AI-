174a81afe44143c2918ad1276fae79badec2eddc
yangxiaosheng6 <3041738953@example.com>
2026-05-28 17:16:33 +0800

    use PORT env for railway

--- server.py
+++ server.py
@@ -516,4 +516,5 @@ with app.app_context():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
